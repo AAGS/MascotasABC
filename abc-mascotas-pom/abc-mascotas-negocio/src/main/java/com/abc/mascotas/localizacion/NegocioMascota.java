f69f052c0d25925a28843397bd29ddd33c3c31b6
Omar <omarcz27@gmail.com>
2016-04-18 22:42:28 -0500

    Ajustes sonar

--- abc-mascotas-pom/abc-mascotas-negocio/src/main/java/com/abc/mascotas/localizacion/NegocioMascota.java
+++ abc-mascotas-pom/abc-mascotas-negocio/src/main/java/com/abc/mascotas/localizacion/NegocioMascota.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import com.abc.mascotas.dtos.InformacionMascotaDto;
@@ -20,7 +21,7 @@ public class NegocioMascota {
 
 	private static final Logger LOG = Logger.getLogger(NegocioMascota.class);
 	private static final String GUARDAR = "guardar";
-	
+
 	@PersistenceContext(unitName = "MascotasPU")
 	private EntityManager em;
 
@@ -29,42 +30,46 @@ public class NegocioMascota {
 
 	@EJB
 	private NegocioLog negocioLog;
-	
+
 	public Boolean guardar(InformacionMascotaDto informacionMascotaDto) {
 		Boolean exito = false;
+		Boolean validacion = true;
 		Punto punto = new Punto();
 		LogPeticionesFallidas logPeticionesFallidas = null;
+
 		try {
 			Ruta ruta = negocioRuta.obtenerRutaPorId(informacionMascotaDto.getIdRuta());
+			String mensaje = "";
 			if (ruta == null) {
-				logPeticionesFallidas = construirObjetoLog(GUARDAR,informacionMascotaDto.toString(), "Ruta no valida");
-				negocioLog.guardar(logPeticionesFallidas);
-				return false;
-			}else if(!Utilidades.esValidoLongitud(informacionMascotaDto.getLongitud())){
-				logPeticionesFallidas = construirObjetoLog(GUARDAR,informacionMascotaDto.toString(), "Longitud no valida");
-				negocioLog.guardar(logPeticionesFallidas);
-				return false;
-			}else if(!Utilidades.esValidoLatitud(informacionMascotaDto.getLatitud())){
-				logPeticionesFallidas = construirObjetoLog(GUARDAR,informacionMascotaDto.toString(), "Latitud no valida");
-				negocioLog.guardar(logPeticionesFallidas);
-				return false;
-			}else if(!Utilidades.esCorrectoRitmoCardiaco(informacionMascotaDto.getRitmoCardiaco())){
-				logPeticionesFallidas = construirObjetoLog(GUARDAR,informacionMascotaDto.toString(), "Ritmo Cardiaco no valido");
-				negocioLog.guardar(logPeticionesFallidas);
-				return false;
-			}else if(!Utilidades.esCorrectoFrecuenciaRespiratoria(informacionMascotaDto.getFrecuenciaRespiratoria())){
-				logPeticionesFallidas = construirObjetoLog(GUARDAR,informacionMascotaDto.toString(), "Frecuencia Respiratoria no valida");
-				negocioLog.guardar(logPeticionesFallidas);
-				return false;
+				mensaje = "Ruta no valida";
+				validacion = false;
+			} else if (!Utilidades.esValidoLongitud(informacionMascotaDto.getLongitud())) {
+				mensaje = "Longitud no valida";
+				validacion = false;
+			} else if (!Utilidades.esValidoLatitud(informacionMascotaDto.getLatitud())) {
+				mensaje = "Latitud no valida";
+				validacion = false;
+			} else if (!Utilidades.esCorrectoRitmoCardiaco(informacionMascotaDto.getRitmoCardiaco())) {
+				mensaje = "Ritmo Cardiaco no valido";
+				validacion = false;
+			} else if (!Utilidades.esCorrectoFrecuenciaRespiratoria(informacionMascotaDto.getFrecuenciaRespiratoria())) {
+				mensaje = "Frecuencia Respiratoria no valida";
+				validacion = false;
 			}
-			
-			mappingDtoToPunto(informacionMascotaDto, punto, ruta);
-			em.persist(punto);
-			exito = true;
+
+			if (!validacion) {
+				logPeticionesFallidas = construirObjetoLog(GUARDAR, informacionMascotaDto.toString(), mensaje);
+				negocioLog.guardar(logPeticionesFallidas);
+			} else {
+				mappingDtoToPunto(informacionMascotaDto, punto, ruta);
+				em.persist(punto);
+				exito = true;
+			}
 		} catch (Exception e) {
-			logPeticionesFallidas = construirObjetoLog("guardar",informacionMascotaDto.toString(), "Error excepcion "+e.getMessage());
+			logPeticionesFallidas = construirObjetoLog("guardar", informacionMascotaDto.toString(),
+					"Error excepcion " + e.getMessage());
 			negocioLog.guardar(logPeticionesFallidas);
-			LOG.error("Error "+e.getMessage());
+			LOG.log(Level.ERROR, "Error " + e.getMessage(), e);
 		}
 
 		return exito;
@@ -101,21 +106,23 @@ public class NegocioMascota {
 				resultadoMascotaDto.setNombreRuta(p.getRuta().getNombre());
 				resultadoMascotaDto.setIdPosicion(p.getId());
 			} else {
-				logPeticionesFallidas = construirObjetoLog("consultarPosicionActual", idRuta.toString(), "No hay posiscion para esta mascota");
+				logPeticionesFallidas = construirObjetoLog("consultarPosicionActual", idRuta.toString(),
+						"No hay posiscion para esta mascota");
 				negocioLog.guardar(logPeticionesFallidas);
 				resultadoMascotaDto.setCodigoMensaje(2);
 				resultadoMascotaDto.setMensaje("No hay posicion para esta mascota");
 			}
 		} else {
-			logPeticionesFallidas = construirObjetoLog("consultarPosicionActual", idRuta.toString(), "La ruta no existe en el sistema");
+			logPeticionesFallidas = construirObjetoLog("consultarPosicionActual", idRuta.toString(),
+					"La ruta no existe en el sistema");
 			negocioLog.guardar(logPeticionesFallidas);
 			resultadoMascotaDto.setCodigoMensaje(1);
 			resultadoMascotaDto.setMensaje("La ruta no existe en el sistema");
 		}
 		return resultadoMascotaDto;
 	}
-	
-	private static LogPeticionesFallidas construirObjetoLog(String metodo, String parametros, String mensaje){
+
+	private static LogPeticionesFallidas construirObjetoLog(String metodo, String parametros, String mensaje) {
 		LogPeticionesFallidas logPeticionesFallidas = new LogPeticionesFallidas();
 		logPeticionesFallidas.setMetodo(metodo);
 		logPeticionesFallidas.setFecha(Calendar.getInstance());
--- abc-mascotas-pom/abc-mascotas-util/src/main/java/com/abc/mascotas/util/Utilidades.java
+++ abc-mascotas-pom/abc-mascotas-util/src/main/java/com/abc/mascotas/util/Utilidades.java
@@ -1,7 +1,7 @@
 package com.abc.mascotas.util;
 
 public class Utilidades {
-	
+
 	public static final double LONGITUD_MAX = 94;
 	public static final double LONGITUD_MIN = -91;
 	public static final double LATITUD_MAX = 95;
@@ -11,44 +11,31 @@ public class Utilidades {
 	public static final double FRECUENCIA_RESPIRATORIA_MIN = 10;
 	public static final double FRECUENCIA_RESPIRATORIA_MAX = 80;
 
-	public static boolean esValidoLongitud(double longitud){
-		if (longitud >= LONGITUD_MIN && longitud <= LONGITUD_MAX){
-			return true;
-		}else{
-			return false;
-		}
+	private Utilidades() {
 	}
-	
-	public static boolean esValidoLatitud(double latitud){
-		if (latitud >= LATITUD_MAX && latitud <= LATITUD_MAX){
-			return true;
-		}else{
-			return false;
-		}
+
+	public static boolean esValidoLongitud(double longitud) {
+		return longitud >= LONGITUD_MIN && longitud <= LONGITUD_MAX;
 	}
-	
-	public static boolean esCorrectoRitmoCardiaco(String ritmoCardiaco){
-		try{
+
+	public static boolean esValidoLatitud(double latitud) {
+		return latitud > LATITUD_MAX && latitud <= LATITUD_MAX;
+	}
+
+	public static boolean esCorrectoRitmoCardiaco(String ritmoCardiaco) {
+		try {
 			double ritmo = Double.parseDouble(ritmoCardiaco);
-			if (ritmo >= RITMO_CARDIACO_MIN && ritmo <= RITMO_CARDIACO_MAX){
-				return true;
-			}else{
-				return false;
-			}
-		}catch(NumberFormatException nfe){
+			return ritmo >= RITMO_CARDIACO_MIN && ritmo <= RITMO_CARDIACO_MAX;
+		} catch (NumberFormatException nfe) {
 			return false;
 		}
 	}
-	
-	public static boolean esCorrectoFrecuenciaRespiratoria(String frecuenciaRespiratoria){
-		try{
+
+	public static boolean esCorrectoFrecuenciaRespiratoria(String frecuenciaRespiratoria) {
+		try {
 			double frecuencia = Double.parseDouble(frecuenciaRespiratoria);
-			if (frecuencia >= FRECUENCIA_RESPIRATORIA_MIN && frecuencia <= FRECUENCIA_RESPIRATORIA_MIN){
-				return true;
-			}else{
-				return false;
-			}
-		}catch(NumberFormatException nfe){
+			return frecuencia > FRECUENCIA_RESPIRATORIA_MIN && frecuencia <= FRECUENCIA_RESPIRATORIA_MIN;
+		} catch (NumberFormatException nfe) {
 			return false;
 		}
 	}
